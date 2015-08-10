ebfd076e18358e3d357a84fac4a85f44cf68dd5b
seshin <transiencymail@gmail.com>
2015-08-09 17:54:01 -0700

    fii

--- webui/config/routes.rb
+++ webui/config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :machines, :has_many => :instances, :has_many => :boxes, :has_many => :configs, :has_many => :connections, :has_many => :harddisks, :has_many => :logfiles, :has_many => :statistics, :has_many => :userlogs
+  resources :instances, :has_many => :bans
+  resources :boxes, :has_many => :boxusers
+
+  get 'home', :to => 'home#index'
+  get 'statistics', :to => 'statistics#index'
+  get 'products', :to => 'products#index'
+  get 'pricing', :to => 'pricing#index'
+  get 'about', :to => 'about#index'
+  get 'contact', :to => 'contact#index'
+  get 'profile(/)', :to => 'profile#index'
+  get 'profile/edit', :to => 'profile#edit'
+  match 'profile/login', :to => 'profile#login', :via => [:get, :post]
+  get 'profile/logout', :to => 'profile#logout'
+  match 'profile/change_password', :to => 'profile#change_password', :via => [:get, :post]
+  match 'profile/signup', :to => 'profile#signup', :as => :signup, :via => [:get, :post]
+  #match 'profile/switchyard/:gucid', :to => 'profile#switchyard'
+  get 'confs/edit', to: 'confs#edit'
+  patch 'confs/update', to: 'confs#update'
+  get 'configs/edit', to: 'configss#edit'
+  patch 'configs/update', to: 'configs#update'
+  #get 'simple_captcha' , to: 'simple_captcha#show', as: "simple_captcha", require: 'simple_captcha'
+  get 'console', to: 'console'
+  get 'console/open', to: 'console#open'
+  post 'console/terminal', to: 'console#terminal'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
